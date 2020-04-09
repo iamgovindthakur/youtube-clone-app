2ce7bdcb09251e1bb44d96a3bc917f4fe678ea64
Govind Thakur <iamgovindthakur@gmail.com>
2020-04-09 12:30:08 +0530

    Updates

---
+++ precache-manifest.a3a6a1b9028f054693b3ab9c621c79c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7df4918b2532b89977f46c0d100b240",
+    "url": "/youtube-clone-app/index.html"
+  },
+  {
+    "revision": "efd90af5f721b706808f",
+    "url": "/youtube-clone-app/static/css/main.6a3ebaf6.chunk.css"
+  },
+  {
+    "revision": "999350962897af99a107",
+    "url": "/youtube-clone-app/static/js/2.c074e178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-clone-app/static/js/2.c074e178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd90af5f721b706808f",
+    "url": "/youtube-clone-app/static/js/main.44095829.chunk.js"
+  },
+  {
+    "revision": "db62881f87e17d5af273",
+    "url": "/youtube-clone-app/static/js/runtime-main.9508b6cd.js"
+  }
+]);
